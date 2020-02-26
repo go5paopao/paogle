6618d7ae840b30c5ab0bbbc83d895275586ca16b
go5paopao <go5kuramubon@gmail.com>
2020-02-26 23:23:08 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="paogle",
+    version="0.0.1",
+    author="momokan",
+    author_email="go5kuramubon@gmail.com",
+    description="Pao's machine learning utility library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
